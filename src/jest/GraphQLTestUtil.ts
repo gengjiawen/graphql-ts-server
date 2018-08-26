29f9d0e2e6a4d972a73f4ce7b78ad6977aace41e
gengjiawen <technicalcute@gmail.com>
2018-08-26 17:32:20 +0800

    add errors map

--- src/jest/GraphQLTestUtil.ts
+++ src/jest/GraphQLTestUtil.ts
@@ -1,6 +1,11 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 export function graphQLRequest(payload: string) {
   const url = process.env.TEST_HOST as string
   return axios.post(url, { query: payload })
+}
+
+export function mapErrorsMessage(response: AxiosResponse) {
+  const errors: any[] = response.data.errors
+  return errors.map(i => i.message)
 }
