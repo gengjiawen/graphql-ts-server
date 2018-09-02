8eb9a8a88c79df7fc39690316efcc400109c170f
gengjiawen <technicalcute@gmail.com>
2018-09-02 15:59:09 +0800

    add login test

---
+++ src/modules/login/Login.test.ts
@@ -0,0 +1,29 @@
+import { graphQLRequest, mapErrorsMessage } from '../../jest/GraphQLTestUtil'
+import { User } from '../../entity/User'
+import { registerMutation } from '../register/Register.test'
+
+const email = 'jack_login@test.com'
+const password = '123456'
+
+export const loginMutation = (e: string, p: string) => `
+mutation {
+  login(email: "${e}", password: "${p}") {
+    token
+  }
+}`
+
+test('login successfully', async () => {
+  const registerRes: any = await graphQLRequest(registerMutation(email, password))
+  expect(registerRes.data.data.register.token).not.toBeNull()
+  await User.update({ email }, { confirmed: true })
+
+  // login success
+  const loginRes: any = await graphQLRequest(loginMutation(email, password))
+  expect(loginRes.data.data.login.token).not.toBeNull()
+  expect(loginRes.data.data.errors).toBeUndefined()
+
+  // login with wrong password
+  const loginWrongPassRes: any = await graphQLRequest(loginMutation(email, '123'))
+  expect(loginWrongPassRes.data.data.login).toBeNull()
+  expect(mapErrorsMessage(loginWrongPassRes)).toMatchSnapshot()
+})
