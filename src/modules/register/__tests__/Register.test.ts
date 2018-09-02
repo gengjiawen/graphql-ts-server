e0e84a85ac2b90bee48cf3af3b6b43de70eb2605
gengjiawen <technicalcute@gmail.com>
2018-09-02 15:43:50 +0800

    expose register mutation

--- src/modules/register/__tests__/Register.test.ts
+++ src/modules/register/__tests__/Register.test.ts
@@ -13,10 +13,10 @@ afterAll(async () => {
   conn.close()
 })
 
-const email = 'tom@bob.com'
+const email = 'jack_register@test.com'
 const password = '123456'
 
-const mutation = (e: string, p: string) => `
+export const registerMutation = (e: string, p: string) => `
 mutation {
   register(email: "${e}", password: "${p}") {
     token
@@ -24,7 +24,7 @@ mutation {
 }`
 
 test('register user', async () => {
-  const response: any = await graphQLRequest(mutation(email, password))
+  const response: any = await graphQLRequest(registerMutation(email, password))
   expect(response.data.data.register.token).not.toBeNull()
   const users = await User.find({ where: { email } })
   expect(users).toHaveLength(1)
@@ -33,15 +33,15 @@ test('register user', async () => {
   expect(user.password).not.toEqual(password)
 
   // test duplicate email
-  const response2 = await graphQLRequest(mutation(email, password))
+  const response2 = await graphQLRequest(registerMutation(email, password))
   expect(response2.data.data.register).toBeNull()
   expect(response2.data.errors[0].message).toEqual(duplicateEmail)
 
   // catch bad password
-  const response3 = await graphQLRequest(mutation(email, 'ab'))
+  const response3 = await graphQLRequest(registerMutation(email, 'ab'))
   expect(mapErrorsMessage(response3)).toMatchSnapshot('catch bad password')
 
   // catch bad email and password
-  const response4 = await graphQLRequest(mutation('ab', 'cd'))
+  const response4 = await graphQLRequest(registerMutation('ab', 'cd'))
   expect(mapErrorsMessage(response4)).toMatchSnapshot('catch bad email and password')
 })
