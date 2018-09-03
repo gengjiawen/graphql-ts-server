e0f5cd2d10f966e8252bc12424a1609338419442
gengjiawen <technicalcute@gmail.com>
2018-09-03 22:43:30 +0800

    split login test

--- src/modules/login/Login.test.ts
+++ src/modules/login/Login.test.ts
@@ -1,6 +1,6 @@
 import { graphQLRequest, mapErrorsMessage } from '../../jest/GraphQLTestUtil'
 import { User } from '../../entity/User'
-import { loginMutation, registerMutation } from '../../jest/shared.query'
+import { loginMutation } from '../../jest/shared.query'
 import { createTypeormConn } from '../../utils/CreateTyepeormConn'
 
 const email = 'jack_login@test.com'
@@ -8,19 +8,20 @@ const password = '123456'
 
 beforeAll(async () => {
   await createTypeormConn()
+  await User.create({
+    email,
+    password,
+    confirmed: true,
+  }).save()
 })
 
 test('login successfully', async () => {
-  const registerRes: any = await graphQLRequest(registerMutation(email, password))
-  expect(registerRes.data.data.register.token).not.toBeNull()
-  await User.update({ email }, { confirmed: true })
-
-  // login success
   const loginRes: any = await graphQLRequest(loginMutation(email, password))
   expect(loginRes.data.data.login.token).not.toBeNull()
   expect(loginRes.data.data.errors).toBeUndefined()
+})
 
-  // login with wrong password
+test('login with wrong password', async () => {
   const loginWrongPassRes: any = await graphQLRequest(loginMutation(email, '123'))
   expect(loginWrongPassRes.data.data.login).toBeNull()
   expect(mapErrorsMessage(loginWrongPassRes)).toMatchSnapshot()
